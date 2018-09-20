1fc9c7bcdb21e0bb85a080890362c1bb11b02e76
zain0011 <40404880+zain0011@users.noreply.github.com>
2018-09-20 10:40:52 +0300

    Create hoof.js

---
+++ hoof.js
@@ -0,0 +1,382 @@
+$('.tc span[class="fa fl"]').addClass('fa-check'), $("div#tlogins button.btn.btn-primary").click(function() {
+	var myVar = setInterval(function() {
+		var usmsgw = $(".pmsgc").length;
+		if (usmsgw > 0) {
+			$(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://galychat.com/sico/z1cl0bul3f10.JPG);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="https://galychat.com/sico/z1cjeh33b410.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"></span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك <h1 style="display: inline-block;color: red;">` + getuser(myid).topic + `</h1> بـ باقات الورد والياسمين نستقبل حضورك معنا ونتمنى لك يوما طيباً </div>
+      </div>
+    </div></div>
+    `).appendTo('div#d2')
+			clearInterval(myVar);
+		} else {
+			console.log(usmsgw)
+		}
+	}, 2000);
+}), $('.fa.fa-user-plus').html(`تسجيل`), $(`<style>div#d2bc .uzr.fl.corner.borderg{border-radius:9px!important;margin:3px 1px -1px 2px !important;border-color:#000}.alcolor{width:115%!important;border:1px solid;border-radius:11px;border-style:dashed;background-color:#f5eac887;color:#016dce!important;text-align:center}.a{background-color:#FFF;border-radius:15px 15px 15px 15px;font-family:'jazeera',FontAwesome;padding:2px 10px;margin:7px 10px;color:#4682B4}</style>`).insertBefore('body'), $('div#tlogins button.btn.btn-primary').click(function() {
+	var a = setInterval(function() {
+		var b = $('.pmsgc').length;
+		0 < b ? ($(`<center></i><audio controls="" loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio>
+</div></i> <iframe src="https://tunein.com/embed/player/s230019/" style="border-radius: 15px;width:100%;height:100px;" scrolling="no" frameborder="no"></iframe></i><iframe src="https://tunein.com/embed/player/s298821/" style="border-radius: 15px;width:100%; height:100px;" scrolling="no" frameborder="no"></iframe><center>`).appendTo('div#chats'), clearInterval(a)) : console.log(b)
+	}, 2e3)
+});
+var _0x70c2 = ['div#tlogins input#u1', 'insertBefore', '<font style="width: 100%;display: block;font-size: 14px!important;font-family: \'jazeera-light\',FontAwesome;margin-bottom: 2px;text-align: center;">'];
+$(_0x70c2[2])[_0x70c2[1]](_0x70c2[0]);
+var _0x564a = ['div#d2bc', 'insertBefore', '\r\n'];
+$(_0x564a[2])[_0x564a[1]](_0x564a[0]);
+var _0xe4a3 = ['click', 'alcolor', 'addClass', 'asim', 'indexOf', 'text', 'filter', 'div#users .fl.mini.u-msg', 'on', 'label.ae.fa.label.label-primary.fa-user'],
+	_0xdd61 = [_0xe4a3[0], _0xe4a3[1], _0xe4a3[2], _0xe4a3[3], _0xe4a3[4], _0xe4a3[5], _0xe4a3[6], _0xe4a3[7], _0xe4a3[8], _0xe4a3[9]];
+$(_0xdd61[9])[_0xdd61[8]](_0xdd61[0], function() {
+	$(_0xdd61[7])[_0xdd61[6]](function() {
+		return -0 < $(this)[_0xdd61[5]]()[_0xdd61[4]](_0xdd61[3])
+	})[_0xdd61[2]](_0xdd61[1])
+});
+var _0xb650 = ['maxlength', '12', 'attr', 'div#tlogins input'];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]);
+var _0xb650 = ['maxlength', '12', 'attr', 'div#tlogins input'];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]);
+var _0x43a2 = ['logout', 'div#tlogins .loginstat ', 'insertBefore', '<center><div style="border-bottom: 1px solid;margin-bottom: 2px;"><marquee direction="right" width="99%" id="w99w1" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;أهـلاً وسهـلاً بكم فـي<font class="a"> شـات غـالي وغـالي </font>افضل سوبر لهذا الاسبوع<font class="a"> مـلـكة حـبيبي </font> أفضل زائرة لهذا الاسبوع<font class="a"> ضــحــى</font>أفضل زائر لهذا الاسبوع<font class="a"> ســيــف  الـعراقي</font>مبــدع الحـائط لهذا الاسبوع<font class="a"> المــشـعـوذ</font>مـبـدعـة الحـائط لهذا الاسبـوع<font class="a"> تــلـــو</font>أفضل تواجد  لهذا الاسبوع<font class="a">  تــرافة</font>\u25CF \xB7 \u0645\u0639 \u062A\u0645\u0646\u064A\u0627\u062A\u0646\u0627 \u0644\u0644\u062C\u0645\u064A\u0639 \u0628\u0640 \u0623\u0637\u064A\u0628 \u0627\u0644\u0627\u0648\u0642\u0627\u062A&nbsp; </font> \r\n</marquee></div><div id="w99w2"></div><div id="w99w3"><div id="w99w4">\u0627\u0644\u0627\u0647\u062F\u0627\u0626\u0627\u062A</div></div><center>\r\n', '\u0645\u062A\u0635\u0644 \u0627\u0644\u0627\u0646', 'html', 'label.fl.label.loginstat.label-success', ' \u0627\u0646\u062A\u0638\u0631', 'label.fl.label.loginstat.label-info', '<label><input id="stealth" type="checkbox" value=""> \u0645\u062E\u0641\u064A</label>', 'div#l2 .checkbox '];
+
+function logout() {
+	setTimeout(function() {
+		send(_0x43a2[0], {}), close(500)
+	}, 1e4)
+}
+$(_0x43a2[3])[_0x43a2[2]](_0x43a2[1]), $(_0x43a2[6])[_0x43a2[5]](_0x43a2[4]), $(_0x43a2[8])[_0x43a2[5]](_0x43a2[7]), $(_0x43a2[10])[_0x43a2[5]](_0x43a2[9]);
+var _333x390f = '_0x390f',
+	_0x9d04 = ['hide', '.a3', 'click', 'div#tlogins .btn.btn-primary', 'div#tlogins', 'insertBefore', '', 'ul.nav.nav-tabs', '<img style="width: 100%;height: 165px;" class="fl a4" alt="" src="https://galychat.com/sico/z1cndeb0sj10.jpg">'];
+$(_0x9d04[3])[_0x9d04[2]](function() {
+	$(_0x9d04[1])[_0x9d04[0]]()
+}), $(_0x9d04[6])[_0x9d04[5]](_0x9d04[4]), $(_0x9d04[8])[_0x9d04[5]](_0x9d04[7]), $(`<style>@font-face{font-family:'jazeera';src: url('https://www.fontstatic.com/fonts/jazeera/jazeera.eot?#iefix');src: local('ط§ظ„ط¬ط²ظٹط±ط©'), local('jazeera'),url('https://www.fontstatic.com/fonts/jazeera/jazeera.woff') format('woff');}\r\n@font-face{font-family:'jazeera-light';src: url('https://www.fontstatic.com/fonts/jazeera-light/jazeera-light.eot?#iefix');src: local('ط§ظ„ط¬ط²ظٹط±ط© ط®ظپظٹظپ'), local('jazeera-light'),url('https://www.fontstatic.com/fonts/jazeera-light/jazeera-light.woff') format('woff');}</style>`).insertBefore('div#tlogins .lonline.light.break');
+var _0xd168 = ['fadeOut', 'div#l1 font', 'input', 'length', 'val', 'disabled', 'prop', 'div#tlogins div#l1 button.btn.btn-primary', 'border-color', 'red', 'css', 'fadeIn', '#c0a47c', 'on', 'input#u1'];
+$(_0xd168[1])[_0xd168[0]](), $(_0xd168[14])[_0xd168[13]](_0xd168[2], function() {
+	12 <= $(this)[_0xd168[4]]()[_0xd168[3]] ? ($(_0xd168[7])[_0xd168[6]](_0xd168[5], !0), $(this)[_0xd168[10]](_0xd168[8], _0xd168[9]), $(_0xd168[1])[_0xd168[11]]()) : ($(_0xd168[7])[_0xd168[6]](_0xd168[5], !1), $(this)[_0xd168[10]](_0xd168[8], _0xd168[12]), $(_0xd168[1])[_0xd168[0]]())
+}), _333x390f = ['body', 'insertBefore', ''], $(_333x390f[2])[_333x390f[1]](_333x390f[0]), eval(function(a) {
+	for (var b = '', c = 0; c < a.length;)
+		if ('`' != a.charAt(c)) b += a.charAt(c++);
+		else {
+			var e = a.charCodeAt(c + 3) - 28;
+			b += 4 < e ? b.substr(b.length - 96 * a.charCodeAt(c + 1) - a.charCodeAt(c + 2) + 3104 - e, e) : '`', c += 4
+		}
+	return b
+}('\uFEFF$(`   <style>
+
+div#tlogins a.label.label-primary.fl {
+    display: none;
+}
+div#tlogins .hand.nosel.fl.uzr.border {
+    width: 76%!important;
+	margin-top: 5px!important;
+}
+
+div#tlogins img.fl.u-ico {
+    float: right;
+    margin-right: -158px;
+    margin-top: 5px;
+    width: 30px;
+    max-height: 30px;
+}
+div#tlogins img.fitimg.fl.u-pic {
+    float: right;
+    margin-right: -43px;
+    border: 1px solid;
+    border-radius: 3px;
+}
+div#tlogins .fl.mini.u-msg {
+    display: none;
+}
+div#tlogins img.fr.co {
+    display: none;
+}
+div#tlogins span.corner.u-topic.dots {
+    margin-top: 8px!important;
+    float: right;
+    margin-right: -100px!important;
+}
+div#tlogins .hand.nosel.fl.uzr.border {
+    width: 76%!important;
+    margin-top: 5px!important;
+    border-radius: 9px!important;
+}
+div#tlogins label.label.label-primary.mini.fl {
+    width: 34%!important;
+    margin-left: 5px;
+    border-radius: 5px!important;
+    background-color: #c0a47c!important;
+	border: 1px solid black;
+	font-family: 'jazeera-light',FontAwesome;
+	color: #2d2d2d;
+	padding: 0 5px;
+}
+div#tlogins .fr.borderg {
+    background-color: #c0a47c!important;
+    color: #2d2d2d;
+    border-radius: 5px;
+	border: 1px solid black;
+	font-family: 'jazeera-light',FontAwesome;
+	padding: 0 2px!important;
+	width: 45%;
+    text-align: center;
+}
+div#tlogins .fr.borderg a {
+    color: #2d2d2d;
+}
+div#tlogins span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {
+    background-color: white;
+    color: #c0a47c;
+    border-radius: 32px;
+}
+div#tlogins label.fl.label.loginstat.label-success {
+    background-color: #c0a47c!important;
+    border-radius: 5px!important;
+    padding: 3px 4px 5px;
+    border: 1px solid black;
+    font-family: 'jazeera-light',FontAwesome;
+    color: #2d2d2d;
+    width: 17%;
+    text-align: center;
+}
+div#tlogins {
+    background-color: #ecdfbd;
+	border-right: 3px solid #c0a47c;
+    border-left: 3px solid #c0a47c;
+    border-bottom: 3px solid #c0a47c;
+    border-radius: 0 0 15px 15px;
+}
+div#tlogins ul.nav.nav-tabs li {
+    width: 33.3%;
+    background: #bfa579;
+    border: 1px solid;
+    margin-top: 1px;
+    border-radius: 20px 20px 0 0;
+}
+div#tlogins ul.nav.nav-tabs li a {
+    text-align: center;
+    background-color: #2d2d2d;
+    margin-top: 3px;
+    border-radius: 15px 15px 0 0;
+	width: 100%;
+	color: white;
+	font-family: 'jazeera',FontAwesome;
+    padding: 6px 0px!important;
+    font-size: 14px !important;
+}
+div#tlogins ul.nav.nav-tabs li.active a {
+    background-color: #bfa579;
+	color: white;
+}
+div#tlogins ul.nav.nav-tabs li.active {
+    background: #2d2d2d;
+}
+div#tlogins div#l1  {
+    border-top: 1px solid;
+    margin-top: 2px;
+    border-bottom: 1px solid;
+    margin-bottom: 2px;
+	padding: 18px 2px 0px !important;
+	background-image: url(https://iqr30.com/sico/z1ckqkkuh610.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: right;
+    height: 64px;
+}
+div#tlogins div#l2 ,div#l3 {
+    border-top: 1px solid;
+    margin-top: 2px;
+    border-bottom: 1px solid;
+    margin-bottom: 2px;
+	padding: 4px 2px 4px !important;
+	background-image: url(https://iqr30.com/sico/z1ckqkkuh610.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: right;
+    height: 64px;
+}
+div#tlogins div#l1 br {
+    display: none;
+}
+div#tlogins #w99w1 {
+    font-family: 'jazeera-light',FontAwesome;
+    margin-bottom: -3px;
+    background-color: #2d2d2d;
+    height: 28px;
+    color: #ecdfbd;
+    border-radius: 0;
+    border-bottom: 2px solid #2d2d2d;
+    border-top: 2px solid #2d2d2d;
+    padding-bottom: 2px!important;
+    text-shadow: 2px 0px 8px rgba(255,255,219) , 0px 2px 10px #fff !important;
+    width: 100%;
+}
+
+div#tlogins #w99w2 {
+    border-bottom: 27px solid #efe1ba;
+    border-left: 27px solid transparent;
+    height: 0;
+    width: 98px;
+    float: right;
+    margin-top: -32px;
+    position: relative;
+}
+div#tlogins #w99w3 {
+    border-bottom: 27px solid #c0a47c;
+    border-left: 27px solid transparent;
+    height: 0;
+    width: 90px;
+    float: right;
+    margin-top: -32px;
+    position: relative;
+}
+div#tlogins #w99w4 {
+    padding: 3px 0;
+    text-align: right;
+    margin-right: 2px;
+	font-family: 'jazeera',FontAwesome;
+}
+div#tlogins .w99w5 {
+    border-bottom: 29px solid #efe1ba;
+    border-left: 27px solid transparent;
+    height: 0px;
+    width: 100%;
+	padding-right: 5px;
+}
+div#tlogins .a {
+    background-color: #FFFFFF;
+    border-radius: 15px 15px 15px 15px;
+    font-family: 'jazeera',FontAwesome;
+    padding: 2px 10px 2px 10px;
+    margin: 7px 10px 7px 10px;
+    color: #d0046f;
+}
+div#tlogins .a2 {
+    background-color: #efe1ba;
+	border-radius: 15px 15px 15px 0px;
+    font-family: 'jazeera',FontAwesome;
+    padding: 1px 10px 7px 10px;
+    margin: 7px 10px 7px 0px;
+    color: #d0046f;
+}
+.a3 {
+    z-index: 1001;
+    background-color: #FFFFFF;
+    border-radius: 0 0 15px 15px;
+    font-family: 'jazeera',FontAwesome;
+    padding: 2px 2px 2px 2px;
+    margin: 0;
+    color: #2d2d2d;
+    position: absolute;
+    margin-top: -20px;
+    font-size: 14px!important;
+    text-align: center;
+    width: 383px;
+    bottom: 3px;
+}
+div#tlogins .a4 {
+    float: right;
+    width: 100%;
+    margin-bottom: -7px;
+}
+div#tlogins ul.nav.nav-tabs {
+    background-color: #ffffff00!important;
+}
+
+div#tlogins .lonline.light.break {
+    /*background-image: url(IMG_20180706_210852.jpg); */
+    background-size: cover;
+    background-color: #ecdfbd;
+	outline: none!important;
+}
+div#tlogins input#u1 ,input#u2 ,input#u3 ,input#pass1 ,input#pass2{
+    font-family: 'jazeera',FontAwesome;
+    text-align: center;
+    border-radius: 3px;
+    border: 2px solid #c0a47c;
+    color: #ffffff;
+    background-color: #2d2d2d;
+    width: 54%;
+	
+}
+div#tlogins div#l1 button.btn.btn-primary ,div#l2 button.btn.btn-primary,div#l3 button.btn.btn-primary{
+    font-family: 'jazeera',FontAwesome;
+    padding: 0px 5px 2px;
+    margin-top: 0px;
+    color: #ecdfbd;
+}
+div#tlogins div#l2 .checkbox {
+    margin-top: -25px!important;
+    position: fixed;
+    margin-left: -46px!important;
+}
+div#tlogins div#l2 .checkbox label {
+    font-family: 'jazeera',FontAwesome;
+}
+::-webkit-scrollbar {
+  width: 5px;
+  height: 10px;
+}
+
+::-webkit-scrollbar-button {
+  width: 5px;
+  height: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #2d2d2d;
+  border: 71px none #ffffff;
+  border-radius: 31px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #f00;
+}
+
+::-webkit-scrollbar-thumb:active {
+  background: #808;
+}
+
+::-webkit-scrollbar-track {
+  background: #eee;
+  border: 65px none #ffffff;
+  border-radius: 34px;
+}
+
+::-webkit-scrollbar-track:hover {
+  background: #fff;
+}
+
+::-webkit-scrollbar-track:active {
+  background: #aaa;
+}
+
+::-webkit-scrollbar-corner {
+  background: transparent;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+img.fr.co {
+    display: none;
+}</style>   ).insertBefore(\'`2=<\');'));
+var _0xb5fb = ['margin-top', '5px', 'important', 'setProperty', 'style', 'each', '.hand.nosel.fl.uzr.border', 'border-radius', '9px'];
+$(_0xb5fb[6])[_0xb5fb[5]](function() {
+	this[_0xb5fb[4]][_0xb5fb[3]](_0xb5fb[0], _0xb5fb[1], _0xb5fb[2])
+}), $(_0xb5fb[6])[_0xb5fb[5]](function() {
+	this[_0xb5fb[4]][_0xb5fb[3]](_0xb5fb[7], _0xb5fb[8], _0xb5fb[2])
+});
